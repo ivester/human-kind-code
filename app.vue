826796d32cea9ad48edd178fb995d2050c912fbd
Ives Ebneter <ives.ebneter@gmail.com>
2024-11-03 13:15:08 +0100

    🧹 chore: Add Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text text-6xl">
+      Hello world!
+    </h1>
   </div>
 </template>
